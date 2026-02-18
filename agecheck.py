faf4db3bca98fc7e436eb141e9db96e0ed609b9f
varman897 <varmansivarooban2015@gmail.com>
2026-02-17 18:57:04 -0800

    age checker

---
+++ agecheck.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 0:
+ if 10 <= age <= 20:
+        print("You are between 10 and 20 years old.")
+ else:
+        print("You are NOT between 10 and 20 years old.")
+else:
+    print("Invalid age")
